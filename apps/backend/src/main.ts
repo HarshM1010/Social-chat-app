6dd31a9d9d16140bf8e64217af0f80647a002c24
HarshM1010 <mane36429@gmail.com>
2026-01-27 20:54:56 +0530

    made some changes in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: [
-      'http://localhost:3000',
       'https://social-chat-application.vercel.app',
+      'http://localhost:3000',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
